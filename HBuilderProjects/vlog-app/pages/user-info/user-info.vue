561e034ef26fd4e683d248077c155b5240c8f1c3
AnJiu9 <1184838231@qq.com>
2020-12-08 18:28:54 +0800

    修改个人信息其他资料

--- HBuilderProjects/vlog-app/pages/user-info/user-info.vue
+++ HBuilderProjects/vlog-app/pages/user-info/user-info.vue
@@ -177,7 +177,27 @@
 				});
 			},
 			//提交
-			submit(){}
+			submit(){
+				let data = {
+					id: this.user.id,
+					phone: this.user.phone,
+					password: this.user.password,
+					nickname: this.nickname,
+					avatar: this.user.avatar,
+					gender: this.gender,
+					birthday: this.birthday,
+					address: this.pickerText,
+					createTime: this.user.createTime
+				};
+				this.$H.post('/user/update', data).then(res => {
+					console.log(res);
+					this.$store.commit('editUserInfo', data);
+					uni.showToast({
+						title:'修改资料成功',
+						icon:'none'
+					});
+				});
+			}
 		}
 	};
 </script>
